2b3171a7e6506882bad29fd5f4a69dbd6cabf91d
sebastiannilsen <sebasn@stud.ntnu.no>
2023-12-01 18:48:31 +0100

    the terraform script now creates the correct resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,17 +14,65 @@ resource "random_string" "container_name" {
   special = false
 }
 
+resource "azurerm_user_assigned_identity" "managed_identity" {
+  name                = "webapp-mi" # You can customize the name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "webapp-plan" # You can customize the name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_linux_web_app" "web_app" {
+  name                = random_pet.rg_name.id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_app_service_plan.app_service_plan.id
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.managed_identity.id]
+  }
+
+
+  site_config {
+    always_on = true
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"          = azurerm_container_registry.acr.login_server
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr.admin_password
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = random_string.container_name.result
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"  # You can customize the SKU
+}
+
+
 resource "azurerm_container_group" "container" {
   name                = "${var.container_group_name_prefix}-${random_string.container_name.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  ip_address_type     = "Public"
   os_type             = "Linux"
   restart_policy      = var.restart_policy
 
   container {
     name   = "${var.container_name_prefix}-${random_string.container_name.result}"
-    image  = var.image
+    image  = "${azurerm_container_registry.acr.login_server}/${var.image}"
     cpu    = var.cpu_cores
     memory = var.memory_in_gb
 
@@ -33,4 +81,12 @@ resource "azurerm_container_group" "container" {
       protocol = "TCP"
     }
   }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
 }
