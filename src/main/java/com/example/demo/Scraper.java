87fb88d458d952aee1b7dff712b6d68e4c154dd8
sebastiannilsen <sebasn@stud.ntnu.no>
2023-12-06 23:46:30 +0100

    added removed files

---
+++ src/main/java/com/example/demo/Scraper.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+
+public class Scraper {
+  WebDriver driver;
+
+
+
+  public Scraper() {
+    File file = new File("/usr/bin/chromedriver");
+    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+    ChromeOptions options = new ChromeOptions();
+
+    options.addArguments("--window-size=1920,1080");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--headless");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--remote-allow-origins=*");
+    //options.addArguments("--whitelisted-ips=*");
+
+    driver = new ChromeDriver(options);
+
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+    System.out.println(driver.getTitle());
+  }
+
+  public String getTitle() {
+    return driver.getTitle();
+  }
+}
